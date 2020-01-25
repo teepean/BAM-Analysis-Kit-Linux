34fcaeae3c072cd07ba5a67abf2ce1b6018626e6
teepean <tnatkinn@gmail.com>
2020-01-25 23:00:02 +0200

    Fix the previous update

--- console_bam.sh
+++ console_bam.sh
@@ -252,7 +252,7 @@ for A in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X Y M ; do
 			rm -f inchr_tmp.sam
 		fi
 
-		samtools view -@ $BAMKIT_THREADS -bT ref.fa -o reads.bam inchr.sam >/dev/null
+		samtools view -@ $BAMKIT_THREADS -bT ref.fa -o reads.bam inchr.sam >/dev/null 2>&1
 		samtools view -@ $BAMKIT_THREADS -H inchr.bam |/bin/sed '/^@PG/d'|/bin/grep -v SN > header01
 		samtools view -@ $BAMKIT_THREADS -H reads.bam > header02
 		/bin/cat header01 header02 > header
